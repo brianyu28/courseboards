8dcd0ae4fa3e7fcf9b1225fea6f5694ccbde743f
Brian Yu <brianyu28@gmail.com>
2017-07-27 21:15:35 -0400

    First commit

---
+++ application.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
