cc124da4e320e85e6a19a1902e4c4c913ba9544c
Brian Yu <brianyu28@gmail.com>
2017-07-29 00:54:42 -0400

    Add faces to student pages

---
+++ static/js/main.js
@@ -0,0 +1,65 @@
+var POSITIVE = 1;
+var NEUTRAL = 0;
+var NEGATIVE = -1;
+var Face = function(opts) {
+    this.sentiment = opts.sentiment;
+    this.svg = opts.svg;
+    this.cx = opts.cx;
+    this.cy = opts.cy;
+    this.r = opts.r;
+}
+
+Face.prototype.draw = function() {
+    this.face = this.svg.append('circle')
+        .attr('cx', this.cx)
+        .attr('cy', this.cy)
+        .attr('r', this.r);
+
+    this.left_eye = svg.append('circle')
+        .attr('cx', this.cx - 0.35 * this.r)
+        .attr('cy', this.cy - 0.3 * this.r)
+        .attr('r', 0.1 * this.r)
+        .style('fill', 'black');
+
+    this.right_eye = svg.append('circle')
+        .attr('cx', this.cx + 0.35 * this.r)
+        .attr('cy', this.cy - 0.3 * this.r)
+        .attr('r', 0.1 * this.r)
+        .style('fill', 'black');
+
+    if (this.sentiment == POSITIVE) {
+        this.face.style('fill', 'green');
+        
+        // Configure the path of the mouth.
+        // Starting on the left, then to the bottom, then to the right.
+        var path = 'M' + (this.cx - 0.5 * this.r) + ',' + (this.cy + 0.3 * this.r);
+        path += ' Q' + (this.cx) + ',' + (this.cy + 0.8 * this.r);
+        path += ' ' + (this.cx + 0.5 * this.r) + ',' + (this.cy + 0.3 * this.r);
+        this.mouth = svg.append('path')
+            .attr('d', path)
+            .style('stroke-width', 0.1 * this.r)
+            .style('stroke', 'black')
+            .style('fill', 'none');
+    }
+    else if (this.sentiment == NEUTRAL) {
+        this.face.style('fill', 'yellow');
+        this.mouth = svg.append('line')
+            .attr('x1', this.cx - 0.5 * this.r)
+            .attr('y1', this.cy + 0.4 * this.r)
+            .attr('x2', this.cx + 0.5 * this.r)
+            .attr('y2', this.cy + 0.4 * this.r)
+            .style('stroke-width', 0.1 * this.r)
+            .style('stroke', 'black');
+    }
+    else if (this.sentiment == NEGATIVE) {
+        this.face.style('fill', 'red');
+        var path = 'M' + (this.cx - 0.5 * this.r) + ',' + (this.cy + 0.5 * this.r);
+        path += ' Q' + (this.cx) + ',' + (this.cy);
+        path += ' ' + (this.cx + 0.5 * this.r) + ',' + (this.cy + 0.5 * this.r);
+        this.mouth = svg.append('path')
+            .attr('d', path)
+            .style('stroke-width', 0.1 * this.r)
+            .style('stroke', 'black')
+            .style('fill', 'none');
+    }
+}
---
+++ static/js/student.js
@@ -0,0 +1,93 @@
+var board;
+$(document).ready(function() {
+    board = new StudentBoard({
+        'identifier': '#student-board',
+        'width': window.innerWidth,
+        'height': window.innerHeight
+    });
+    board.draw();
+});
+
+var size_timeout;
+$(window).resize(function() {
+    board.redraw(window.innerWidth, window.innerHeight);
+});
+
+var StudentBoard = function(opts) {
+    this.identifier = opts.identifier;
+    this.width = opts.width;
+    this.height = opts.height;
+}
+
+StudentBoard.prototype.draw = function() {
+    var _this = this;
+    this.canvas = d3.select(this.identifier);
+    this.canvas.selectAll('*').remove();
+    canvas = this.canvas;
+
+    this.svg = canvas.append('svg')
+                     .attr('width', this.width)
+                     .attr('height', this.height)
+                     .classed('svg', true);
+    svg = this.svg;
+
+    this.calculate_bounds();
+    this.redraw(this.width, this.height);
+
+}
+
+StudentBoard.prototype.redraw = function(width, height) {
+    this.width = width;
+    this.height = height;
+    this.svg.attr('width', width).attr('height', height);
+    this.svg.selectAll('*').remove();
+    this.calculate_bounds();
+
+    var sentiments = [NEGATIVE, NEUTRAL, POSITIVE];
+    for (var i = 0; i < this.centers.length; i++) {
+        var face = new Face({
+            'sentiment': sentiments[i],
+            'svg': this.svg,
+            'cx': this.centers[i][0],
+            'cy': this.centers[i][1],
+            'r': this.radius
+        });
+        face.draw();
+    }
+}
+
+// Computes the centers of the faces and their size.
+StudentBoard.prototype.calculate_bounds = function() {
+    
+    // Optimally, have at least 10 pixels of pading on each side
+    // of a 300 pixel image.
+    if (this.width >= 960) {
+        var sector = this.width / 3;
+        var cx = sector / 2;
+        var cy = this.height / 2;
+
+        this.centers = [[cx, cy], [cx + sector, cy], [cx + 2 * sector, cy]];
+        this.radius = 150;
+    }
+
+    // Otherwise, for sufficiently large screens,
+    // just scale down based on an expectation of 10
+    // pixels of padding.
+    else if (this.width >= 600) {
+        var sector = this.width / 3;
+        var cx = sector / 2;
+        var cy = this.height / 2;
+
+        this.centers = [[cx, cy], [cx + sector, cy], [cx + 2 * sector, cy]];
+        this.radius = cx - 10;
+    }
+
+    else {
+        var sector = this.height / 3;
+        var cx = this.width / 2;
+        var cy = sector / 2;
+
+        this.centers = [[cx, cy], [cx, cy + sector], [cx, cy + 2 * sector]];
+        this.radius = cy - 10;
+    }
+}
